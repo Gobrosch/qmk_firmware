207706e04d95c64431e6bc7ca499eedd0d4c50b3
Gobrosch <github@gobrosch.de>
2023-01-25 22:16:40 +0100

    Copied rev2 keymap files into folder rev2/

---
+++ keyboards/splitkb/kyria/keymaps/gobrosch/rev2/rules.mk
@@ -0,0 +1,8 @@
+OLED_ENABLE = yes
+OLED_DRIVER = SSD1306      # Enables the use of OLED displays
+ENCODER_ENABLE = yes       # Enables the use of one or more encoders
+RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+
+AUTO_SHIFT_ENABLE = yes
+
+BOOTLOADER = caterina
